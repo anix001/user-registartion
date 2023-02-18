192d6400a6e4d8be0e2335dea522346b6188fa4e
anix001 <chaudharyanish305@gmail.com>
2023-02-18 12:19:21 +0545

    Feat: User registration with  email validation

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender()
+	{
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(25);
+
+		mailSender.setUsername("chaudharyanish305@gmail.com");
+		mailSender.setPassword("pdbqqeanspmnejyk");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+}
